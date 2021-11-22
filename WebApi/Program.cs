4de47f5b7c68018b1404b22d0bca3a097a26fb9d
Georgy <ajvbyud@outlook.com>
2021-11-22 23:44:13 +0300

    ConnectionString null

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,19 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence.MsSql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+string s;
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(s=builder.Configuration.GetConnectionString("DataContext")));
+
+    
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
